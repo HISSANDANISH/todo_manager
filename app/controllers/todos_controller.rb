cbdffaec0ce175713cab800334449a0c781aee5c
HISSANDANISH <hissandanish@gmail.com>
2020-02-26 18:07:06 +0530

    Render list of todos in /todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,6 @@
 # todos_controller.rb
 class TodosController < ApplicationController
   def index
-    render plain: "Hello, this is /todos!"
+    render plain: Todo.all.order(:due_date).map { |todo| todo.to_pleasant }.join("\n")
   end
 end
