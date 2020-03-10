d5279d62936e6bc8628439273f15a7822eaf312c
HISSANDANISH <hissandanish@gmail.com>
2020-03-10 16:50:36 +0530

    Use the resource routing conventions for todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,7 @@
 # todos_controller.rb
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.all.order(:due_date).map { |todo| todo.to_pleasant }.join("\n")
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "todos", to: "todos#index"
-  get "todos/create", to: "todos#create"
-  get "todos/:id", to: "todos#show"
+  resources :todos
 end
