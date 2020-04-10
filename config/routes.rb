31db60f90481f92c5490df64ed309f30c313ddea
HISSANDANISH <hissandanish@gmail.com>
2020-04-10 20:17:22 +0530

    Updated database migrations,by danish

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :todos
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post "users/login", to: "users#check"
+  resources :users
 end
