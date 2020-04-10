9cf8700e1fdffbbc895106625d9a28d4435ef703
HISSANDANISH <hissandanish@gmail.com>
2020-04-10 18:15:56 +0530

    Update database migrations ,by danish

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,7 +3,8 @@ class TodosController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    render plain: Todo.all.order(:due_date).map { |todo| todo.to_pleasant }.join("\n")
+    render "index"
+    #render plain: Todo.all.order(:due_date).map { |todo| todo.to_pleasant }.join("\n")
   end
 
   def show
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,17 @@
 class Todo < ActiveRecord::Base
-  def to_pleasant
-    is_completed = completed ? "[X]" : "[ ]"
-    "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
+  def self.overdue
+    all.where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
   end
 end
