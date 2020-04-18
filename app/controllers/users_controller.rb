8251b0664311b5dbd442e68381540967f61218c3
HISSANDANISH <hissandanish@gmail.com>
2020-04-18 18:00:53 +0530

    Added SignUp and Todo validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    User.create!(
+    new_user = User.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       password: params[:password_digest],
     )
-    redirect_to "/"
+    if new_user.save
+      session[:current_user_id] = new_user.id
+      flash[:success] = "Signed Up successfully & Logged In"
+      redirect_to "/"
+    else
+      flash[:error] = new_user.errors.full_messages.join(", ")
+      redirect_to new_user_path
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :todos
+  validates :first_name, presence: true
+  validates :email, presence: true
 end
